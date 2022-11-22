647f31faa5b1e6e08d5ac75cd7d8468779fae4d6
kionou <kionoumamadou.00@gmail.com>
2022-11-22 10:34:03 +0000

    ok

--- app.js
+++ app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(8080, ()=>{
+  console.log('connecter au port' + '8080');
+});
+
+// module.exports = app;
